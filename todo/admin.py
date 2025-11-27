a8795af1dbecb80c7ad63b066a40fc1567dae070
Divyank <divyankkadam880@gmail.com>
2025-11-27 10:15:16 +0530

    add task funstionality

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
+
+# design layout for admin
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task' , 'is_completed' , 'updated_at' )
+    search_fields = ( 'task' ,)
 
 # Register your models here.
+admin.site.register(Task , TaskAdmin)
