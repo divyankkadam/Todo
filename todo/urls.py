f7c290fd21a76197ba2714775544d2de4ae1208c
Divyank <divyankkadam880@gmail.com>
2025-11-27 23:11:03 +0530

    All Function Update

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Add Task
     path ('addTask/' , views.addTask , name='addTask'),
+    #Mark as done
+    path('mark_as_done/<int:pk>/' , views.mark_as_done , name='mark_as_done'),
+    #Mark as Undone
+    path('mark_as_Undone/<int:pk>/' , views.mark_as_Undone , name='mark_as_Undone'),
+    #Edit Task
+    path('edit_task/<int:pk>/' , views.edit_task , name='edit_task' ),
+    #delete Task
+    path('delete_task/<int:pk>/' , views.delete_task , name='delete_task' ),
+
+
+
 ]
 
--- todo_main/urls.py
+++ todo_main/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.home , name='home'),
+    
 
     #todo
     path('todo/' ,include('todo.urls')),
